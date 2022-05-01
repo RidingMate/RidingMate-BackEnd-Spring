cbb9e6cb2e642a8733b37b5c4ac0accd077ccdaf
proceane <proceane1@gmail.com>
2022-05-02 00:51:00 +0900

    게시판 서비스 인터페이스 생성

--- src/main/java/com/ridingmate/api/service/BoardService.java
+++ src/main/java/com/ridingmate/api/service/BoardService.java
@@ -1,50 +1,19 @@
 package com.ridingmate.api.service;
 
 import com.ridingmate.api.entity.BoardEntity;
-import com.ridingmate.api.entity.NoticeBoardEntity;
-import com.ridingmate.api.entity.TradeBoardEntity;
-import com.ridingmate.api.payload.BoardDto;
-import com.ridingmate.api.payload.NoticeBoardInsertRequest;
-import com.ridingmate.api.payload.TradeBoardInsertRequest;
-import com.ridingmate.api.repository.BoardRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class BoardService {
+public interface BoardService {
 
-    private final BoardRepository boardRepository;
+    void insertBoardContent(BoardEntity board);
 
-    @Transactional
-    public void insertNoticeBoardContent(NoticeBoardInsertRequest request) {
-        NoticeBoardEntity notice = new NoticeBoardEntity(request.getTitle());
-        boardRepository.save(notice);
-    }
+    void updateBoardContent(BoardEntity board);
 
-    @Transactional
-    public void insertTradeBoardContent(TradeBoardInsertRequest request) {
-        TradeBoardEntity trade = new TradeBoardEntity(request.getTitle(), request.getPrice());
-        boardRepository.save(trade);
-    }
+    void deleteBoardContent(Long boardId);
 
-    public void updateBoardContent() {
-        // TODO : 게시글 수정
-    }
+    Page<BoardEntity> getBoardList(PageRequest page);
 
-    public void getNoticeBoardList(int pageNum) {
-        // TODO : 게시판 게시글 리스트 조회
-    }
+    BoardEntity getBoardContent(Long boardId);
 
-    public void getBoardContent() {
-        // TODO : 게시글 상세 조회
-    }
-
-    public void deleteBoardContent() {
-        // TODO : 게시글 삭제
-    }
 }
