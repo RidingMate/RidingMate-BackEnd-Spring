391cfe2fa281f163288cf3573be220b11e7a14bd
proceane <proceane1@gmail.com>
2022-05-05 01:27:48 +0900

    ResponseEntity 처리

--- src/main/java/com/ridingmate/api/controller/BoardController.java
+++ src/main/java/com/ridingmate/api/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.ridingmate.api.controller;
 
+import com.ridingmate.api.consts.ResponseCode;
 import com.ridingmate.api.entity.NoticeBoardEntity;
 import com.ridingmate.api.entity.TradeBoardEntity;
+import com.ridingmate.api.payload.common.ApiResponse;
 import com.ridingmate.api.payload.user.dto.NoticeBoardContentDto;
 import com.ridingmate.api.payload.user.dto.NoticeBoardDto;
 import com.ridingmate.api.payload.user.dto.TradeBoardContentDto;
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,38 +30,38 @@ public class BoardController {
 
     @GetMapping("/notice/list")
     @ApiOperation("공지사항 리스트 조회")
-    public Page<NoticeBoardDto> getNoticeBoardList(
+    public ResponseEntity<Page<NoticeBoardDto>> getNoticeBoardList(
             @RequestParam(value = "pageNum") int pageNum
     ) {
         Sort sort = Sort.by("createAt").descending();
         PageRequest page = PageRequest.of(pageNum - 1, 10, sort);
-        return noticeBoardService.getBoardList(page).map(NoticeBoardDto::convertEntityToDto);
+        return ResponseEntity.ok(noticeBoardService.getBoardList(page).map(NoticeBoardDto::convertEntityToDto));
     }
 
     @GetMapping("/trade/list")
     @ApiOperation("거래글 리스트 조회")
-    public Page<TradeBoardDto> getTradeBoardList(
+    public ResponseEntity<Page<TradeBoardDto>> getTradeBoardList(
             @RequestParam(value = "pageNum") int pageNum
     ) {
         Sort sort = Sort.by("createAt").descending();
         PageRequest page = PageRequest.of(pageNum - 1, 10, sort);
-        return tradeBoardService.getBoardList(page).map(TradeBoardDto::convertEntityToDto);
+        return ResponseEntity.ok(tradeBoardService.getBoardList(page).map(TradeBoardDto::convertEntityToDto));
     }
 
 
     @PostMapping("/notice")
     @ApiOperation("공지사항 등록")
-    public String insertNoticeBoard(
+    public ResponseEntity insertNoticeBoard(
             @RequestBody NoticeBoardRequest request
     ) {
         NoticeBoardEntity noticeBoard = new NoticeBoardEntity(request.getTitle());
         noticeBoardService.insertBoardContent(noticeBoard);
-        return "success";
+        return ResponseEntity.ok(new ApiResponse(ResponseCode.SUCCESS));
     }
 
     @PostMapping("/trade")
     @ApiOperation("거래글 등록")
-    public String insertTradeBoard(
+    public ResponseEntity insertTradeBoard(
             @RequestBody TradeBoardRequest request
     ) {
         // TODO : 내 바이크 조건처리 필요
@@ -73,18 +76,18 @@ public class BoardController {
                 request.getMileage(),
                 request.getPrice());
         tradeBoardService.insertBoardContent(tradeBoard);
-        return "success";
+        return ResponseEntity.ok(new ApiResponse(ResponseCode.SUCCESS));
     }
 
     @GetMapping("/notice/{boardId}")
     @ApiOperation("공지사항 상세 조회")
-    public NoticeBoardContentDto getNoticeBoardContent(@PathVariable("boardId") Long boardId) {
-        return NoticeBoardContentDto.convertEntityToDto(noticeBoardService.getBoardContent(boardId));
+    public ResponseEntity<NoticeBoardContentDto> getNoticeBoardContent(@PathVariable("boardId") Long boardId) {
+        return ResponseEntity.ok(NoticeBoardContentDto.convertEntityToDto(noticeBoardService.getBoardContent(boardId)));
     }
 
     @GetMapping("/trade/{boardId}")
     @ApiOperation("거래글 상세 조회")
-    public TradeBoardContentDto getTradeBoardContent(@PathVariable("boardId") Long boardId) {
-        return TradeBoardContentDto.convertEntityToDto(tradeBoardService.getBoardContent(boardId));
+    public ResponseEntity<TradeBoardContentDto> getTradeBoardContent(@PathVariable("boardId") Long boardId) {
+        return ResponseEntity.ok(TradeBoardContentDto.convertEntityToDto(tradeBoardService.getBoardContent(boardId)));
     }
 }
