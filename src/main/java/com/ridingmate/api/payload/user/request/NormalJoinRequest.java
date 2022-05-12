1396445679440fce3be8b734eb0ffb15c97ff418
proceane <proceane1@gmail.com>
2022-05-12 13:36:11 +0900

    ApiModelProperty 추가

--- src/main/java/com/ridingmate/api/payload/user/request/NormalJoinRequest.java
+++ src/main/java/com/ridingmate/api/payload/user/request/NormalJoinRequest.java
@@ -1,5 +1,6 @@
 package com.ridingmate.api.payload.user.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
@@ -8,11 +9,14 @@ import javax.validation.constraints.NotEmpty;
 public class NormalJoinRequest {
 
     @NotEmpty(message = "아이디는 필수입니다.")
+    @ApiModelProperty(value = "유저 ID", required = true)
     private String userId;
 
     @NotEmpty(message = "비밀번호는 필수입니다.")
+    @ApiModelProperty(value = "비밀번호", required = true)
     private String password;
 
     @NotEmpty(message = "닉네임은 필수입니다.")
+    @ApiModelProperty(value = "닉네임", required = true)
     private String nickname;
 }
--- src/main/java/com/ridingmate/api/payload/user/request/NoticeBoardInsertRequest.java
+++ src/main/java/com/ridingmate/api/payload/user/request/NoticeBoardInsertRequest.java
@@ -1,10 +1,13 @@
 package com.ridingmate.api.payload.user.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class NoticeBoardInsertRequest {
+
+    @ApiModelProperty(value = "공지사항 게시글 제목", required = true)
     private String title;
 }
