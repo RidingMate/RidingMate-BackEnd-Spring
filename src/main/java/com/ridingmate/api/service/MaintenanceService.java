7dfda38969be69dbe0fdd0f754430a1804fea10d
hyein-song <shi9512@naver.com>
2022-05-23 22:11:17 +0900

    MaintenanceService method에 transactional annotation 추가

--- src/main/java/com/ridingmate/api/service/MaintenanceService.java
+++ src/main/java/com/ridingmate/api/service/MaintenanceService.java
@@ -10,6 +10,7 @@ import com.ridingmate.api.repository.MaintenanceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +21,7 @@ public class MaintenanceService {
 
     private final MaintenanceRepository maintenanceRepository;
 
+    @Transactional
     public List<MaintenanceResponse> getMaintenanceList(Long bike_id) {
         List<MaintenanceEntity> maintenanceEntities = maintenanceRepository.findByBikeIdx(bike_id);
         return maintenanceEntities.stream().map(maintenanceEntity ->
@@ -27,11 +29,13 @@ public class MaintenanceService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public MaintenanceResponse getMaintenanceDetail(Long bike_id, Long maintenance_id){
         MaintenanceEntity maintenanceEntity = maintenanceRepository.findByBikeIdxAndIdx(bike_id,maintenance_id);
         return new MaintenanceResponse().convertEntityToResponse(maintenanceEntity);
     }
 
+    @Transactional
     public ResponseEntity<ApiResponse> insertMaintenance(MaintenanceInsertRequest request){
         MaintenanceEntity maintenanceEntity = MaintenanceEntity.createMaintenance(request);
         maintenanceRepository.save(maintenanceEntity);
@@ -39,6 +43,7 @@ public class MaintenanceService {
         return ResponseEntity.ok(new ApiResponse(ResponseCode.SUCCESS));
     }
 
+    @Transactional
     public ResponseEntity<ApiResponse> updateMaintenance(MaintenanceUpdateRequest request){
         // 바이크정보
         MaintenanceEntity maintenanceEntity = maintenanceRepository.findByIdxAndBike(request.getIdx(),request.getBike());
