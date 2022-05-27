97619080866920b226f524909d0fdf3a6eda0733
proceane <proceane1@gmail.com>
2022-05-27 22:59:34 +0900

    안쓰는 import 제거

--- src/main/java/com/ridingmate/api/controller/user/BoardController.java
+++ src/main/java/com/ridingmate/api/controller/user/BoardController.java
@@ -1,5 +1,19 @@
 package com.ridingmate.api.controller.user;
 
+import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ridingmate.api.consts.ResponseCode;
 import com.ridingmate.api.entity.LocationEntity;
 import com.ridingmate.api.entity.NoticeBoardEntity;
@@ -21,16 +35,6 @@ import com.ridingmate.api.service.common.AuthService;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
 /*
     중고장터 관련 컨트롤러
     필터 :   제조사
