820ee55be023bb046a831864456ef2c7ee562d6c
proceane <proceane1@gmail.com>
2022-05-29 11:16:01 +0900

    거래글 inner dto 사용 메소드 추가

--- src/main/java/com/ridingmate/api/repository/predicate/BoardPredicate.java
+++ src/main/java/com/ridingmate/api/repository/predicate/BoardPredicate.java
@@ -7,6 +7,7 @@ import org.springframework.util.StringUtils;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.ridingmate.api.entity.value.TradeStatus;
+import com.ridingmate.api.payload.user.dto.BoardDto;
 import com.ridingmate.api.payload.user.request.TradeSearchRequest;
 
 public class BoardPredicate {
@@ -27,6 +28,27 @@ public class BoardPredicate {
         builder.and(tradeBoardEntity.year.between(request.getMinYear(), request.getMaxYear()));
         builder.and(tradeBoardEntity.cc.between(request.getMinCc(), request.getMaxCc()));
         if (request.getHideComplete().equals("Y")) {
+            builder.and(tradeBoardEntity.status.ne(TradeStatus.COMPLETED));
+        }
+        return builder;
+    }
+
+    public static Predicate tradeBoardPredicate(BoardDto.Request.TradeList dto) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(dto.getCompany())) {
+            builder.and(tradeBoardEntity.company.eq(dto.getCompany()));
+        }
+        if (StringUtils.hasText(dto.getModelName())) {
+            builder.and(tradeBoardEntity.modelName.eq(dto.getModelName()));
+        }
+        if (StringUtils.hasText(dto.getLocationCode())) {
+            builder.and(tradeBoardEntity.location().locationCode.eq(dto.getLocationCode()));
+        }
+        builder.and(tradeBoardEntity.mileage.between(dto.getMinMileage(), dto.getMaxMileage()));
+        builder.and(tradeBoardEntity.price.between(dto.getMinPrice(), dto.getMaxPrice()));
+        builder.and(tradeBoardEntity.year.between(dto.getMinYear(), dto.getMaxYear()));
+        builder.and(tradeBoardEntity.cc.between(dto.getMinCc(), dto.getMaxCc()));
+        if (dto.getHideComplete().equals("Y")) {
             builder.and(tradeBoardEntity.status.ne(TradeStatus.COMPLETED));
         }
         return builder;
--- src/main/java/com/ridingmate/api/service/TradeBoardService.java
+++ src/main/java/com/ridingmate/api/service/TradeBoardService.java
@@ -8,13 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.querydsl.core.types.Predicate;
 import com.ridingmate.api.consts.ResponseCode;
 import com.ridingmate.api.entity.BoardEntity;
+import com.ridingmate.api.entity.LocationEntity;
 import com.ridingmate.api.entity.TradeBoardEntity;
+import com.ridingmate.api.entity.UserEntity;
 import com.ridingmate.api.exception.CustomException;
+import com.ridingmate.api.payload.user.dto.BoardDto;
 import com.ridingmate.api.payload.user.dto.TradeBoardDto;
 import com.ridingmate.api.payload.user.request.TradeSearchRequest;
 import com.ridingmate.api.repository.BoardRepository;
 import com.ridingmate.api.repository.TradeBoardRepository;
 import com.ridingmate.api.repository.predicate.BoardPredicate;
+import com.ridingmate.api.service.common.AuthService;
 import com.ridingmate.api.service.common.AwsS3Service;
 
 import lombok.RequiredArgsConstructor;
@@ -26,11 +30,42 @@ public class TradeBoardService implements BoardService {
     private final BoardRepository boardRepository;
     private final TradeBoardRepository tradeBoardRepository;
     private final AwsS3Service s3Service;
+    private final AuthService authService;
+    private final LocationService locationService;
 
     @Transactional
     public void insertBoardContent(BoardEntity board) {
         // TODO : 파일 등록 로직 추가
         boardRepository.save(board);
+    }
+
+    public void insertTradeBoardContent(BoardDto.Request.TradeInsert dto) {
+        // TODO : 내 바이크 조건처리 필요
+        UserEntity userEntity = authService.getUserEntityByAuthentication();
+
+        // 거래 지역
+        LocationEntity location = null;
+        if (dto.getLocationCode() != null) {
+            location = locationService.getLocation(dto.getLocationCode());
+        }
+
+        TradeBoardEntity tradeBoard = new TradeBoardEntity(
+                dto.getTitle(),
+                dto.getContent(),
+                dto.getCompany(),
+                dto.getModelName(),
+                dto.getFuelEfficiency(),
+                dto.getCc(),
+                dto.getYear(),
+                dto.getMileage(),
+                dto.getPrice(),
+                dto.getPhoneNumber(),
+                dto.getIsOpenToBuyer(),
+                dto.getPurchaseYear(),
+                dto.getPurchaseMonth(),
+                userEntity,
+                location);
+        insertBoardContent(tradeBoard);
     }
 
     @Transactional
@@ -47,12 +82,37 @@ public class TradeBoardService implements BoardService {
                 .map(tradeBoard -> new TradeBoardDto(tradeBoard));
     }
 
+    public Page<BoardDto.Response.TradeList> getTradeList(Pageable pageable, BoardDto.Request.TradeList dto) {
+        return getBoardList(pageable, BoardPredicate.tradeBoardPredicate(dto))
+                .map(tradeBoard -> BoardDto.Response.TradeList.builder()
+                                                              .id(tradeBoard.getIdx())
+                                                              .company(tradeBoard.getCompany())
+                                                              .modelName(tradeBoard.getModelName())
+                                                              .price(tradeBoard.getPrice())
+                                                              .cc(tradeBoard.getCc())
+                                                              .mileage(tradeBoard.getMileage())
+                                                              .year(tradeBoard.getYear())
+                                                              .build());
+    }
+
     @Transactional
     public TradeBoardEntity getBoardContent(Long boardId) {
         BoardEntity board = boardRepository.findById(boardId).orElseThrow(() ->
                 new CustomException(ResponseCode.NOT_FOUND_BOARD));
         board.increaseHitCount();
         return (TradeBoardEntity) board;
+    }
+
+    public BoardDto.Response.TradeContent getTradeBoardContent(Long boardId) {
+        TradeBoardEntity board = getBoardContent(boardId);
+        return BoardDto.Response.TradeContent.builder()
+                                             .company(board.getCompany())
+                                             .modelName(board.getModelName())
+                                             .price(board.getPrice())
+                                             .cc(board.getCc())
+                                             .mileage(board.getMileage())
+                                             .year(board.getYear())
+                                             .build();
     }
 
     @Transactional
