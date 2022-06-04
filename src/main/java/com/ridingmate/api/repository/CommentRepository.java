e46f376bd7be5b9e32c837262ca787a7475ed387
proceane <proceane1@gmail.com>
2022-06-04 21:43:28 +0900

    댓글 등록

---
+++ src/main/java/com/ridingmate/api/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.ridingmate.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.ridingmate.api.entity.CommentEntity;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long>, QuerydslPredicateExecutor<CommentEntity> {
+}
