36c6fe1837a2116c47f744c2e42e82b9f5bdc828
hyein-song <shi9512@naver.com>
2022-06-16 00:38:03 +0900

    정비 기록 리스트 연도별 취합 구현

--- src/main/java/com/ridingmate/api/payload/user/request/MaintenanceInsertRequest.java
+++ src/main/java/com/ridingmate/api/payload/user/request/MaintenanceInsertRequest.java
@@ -25,7 +25,7 @@ public class MaintenanceInsertRequest {
     private String location;
 
     @ApiModelProperty(value = "가격", required = true)
-    private String amount;
+    private int amount;
 
     // TODO : 사진 기록해야함 -> 멀티파트를 이용한 썸네일 저장
     // TODO : BLOB같은거 이용해서 내용에 사진도 들어갈 수 있게 저장
