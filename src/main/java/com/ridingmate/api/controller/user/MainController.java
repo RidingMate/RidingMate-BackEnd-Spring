ac319d6d924785fc19f2d2a46ea86e09aa1a73e2
kaaang <kaaang@kakao.com>
2022-06-15 17:56:04 +0900

    메인 컨트롤러

--- src/main/java/com/ridingmate/api/controller/user/MainController.java
+++ src/main/java/com/ridingmate/api/controller/user/MainController.java
@@ -1,8 +1,13 @@
 package com.ridingmate.api.controller.user;
 
+import com.ridingmate.api.payload.common.ApiResponse;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 /*
     메인 뷰 관러 컨트롤러
@@ -13,9 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/fuel")
+@RequestMapping("/v1/main")
 public class MainController {
 
 
+    @GetMapping("")
+    @ApiOperation(value = "웹 메인 컨트롤러")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "user 토큰", defaultValue = "null", dataType = "String", required = true),
+    })
+    public void webHome(){
+
+    }
+
+    @GetMapping("")
+    @ApiOperation(value = "앱 메인 컨트롤러")
+    public void appHome(){
+
+    }
 
 }
