f844a9b263a8002c209cc3bdf80246b9ad6da214
kaaang <kaaang@kakao.com>
2022-06-15 17:57:02 +0900

    메인 컨트롤러인

--- src/main/java/com/ridingmate/api/controller/user/MainController.java
+++ src/main/java/com/ridingmate/api/controller/user/MainController.java
@@ -1,19 +1,13 @@
 package com.ridingmate.api.controller.user;
 
-import com.ridingmate.api.payload.common.ApiResponse;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /*
-    메인 뷰 관러 컨트롤러
 
-    상단배너, 하단배너 가져와야함
-    중고거래 top8 가져와야함
 */
 
 @RestController
