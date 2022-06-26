144881a07eb8ed887c53113a50428440ea704a80
proceane <proceane1@gmail.com>
2022-06-26 14:32:43 +0900

    애플 OAuth2 유저 객체 생성

---
+++ src/main/java/com/ridingmate/api/security/AppleOAuth2UserInfo.java
@@ -0,0 +1,20 @@
+package com.ridingmate.api.security;
+
+import java.util.Map;
+
+public class AppleOAuth2UserInfo extends OAuth2UserInfo {
+
+    public AppleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
