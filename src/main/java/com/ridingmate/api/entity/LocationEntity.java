a1630f6d881fc0b367755966df0abfd8b4e75904
proceane <proceane1@gmail.com>
2022-07-21 22:32:17 +0900

    Clean code

--- src/main/java/com/ridingmate/api/entity/LocationEntity.java
+++ src/main/java/com/ridingmate/api/entity/LocationEntity.java
@@ -1,15 +1,8 @@
 package com.ridingmate.api.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
--- src/main/java/com/ridingmate/api/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ridingmate/api/security/JwtAuthenticationEntryPoint.java
@@ -1,17 +1,17 @@
 package com.ridingmate.api.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
