a4dc81194a8894ef6c09b0cac9423eddae9447de
kaaang <kaaang@kakao.com>
2022-07-21 19:11:35 +0900

    test 시작

--- src/test/java/com/ridingmate/api/service/BikeServiceTest.java
+++ src/test/java/com/ridingmate/api/service/BikeServiceTest.java
@@ -1,25 +1,29 @@
 package com.ridingmate.api.service;
 
+import com.ridingmate.api.exception.CustomException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
-@ActiveProfiles("dev")
+@ActiveProfiles("local")
 public class BikeServiceTest {
 
     @Autowired
-    private BikeService bikeService;
+    BikeService bikeService;
 
-    @DisplayName("null 입력")
     @Test
-    public void nullInput(){
-        Assertions.assertNull(bikeService.searchModel(null));
+    public void bikeModelSearch(){
+        Assertions.assertThrows(Exception.class, ()->
+                bikeService.searchModel("zzz")
+        );
+//        bikeService.searchModel("G 310 R");
     }
 
 }
